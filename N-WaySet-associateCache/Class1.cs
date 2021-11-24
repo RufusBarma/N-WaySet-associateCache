7291ce62959aff59c7f51f1af13804a6f552c966
RufusBarma <neymanyu@gmail.com>
2021-11-24 22:06:57 +0500

    Init commit

---
+++ N-WaySet-associateCache/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace N_WaySet_associateCache
+{
+    public class Class1
+    {
+    }
+}
---
+++ N-WaySet-associateCacheTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace N_WaySet_associateCacheTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
